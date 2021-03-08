ce32c7b58b617ecb6e77b06f3208a46284d495df
Ahmed Kotb <ahmedmohamed.kotb@yahoo.com>
2021-03-08 16:46:29 +0200

    ADD Payment

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chekout from "./Chekout";
 import Login from "./Login";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51ISfs7A1PCyt9YQPnr0388y9ePwJqQ3n2FoTA69B0qIpQFPxBXM10vMKRZAkTlN6ar0bGjzUASWMQ9j6gH4oNvfE00dkzDW7Hq"
+);
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-        <Route path="/login">
-            <Login/>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/checkout">
             <Header />
             <Chekout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
